b40b7209e5f87926ab5fda816bcc26036c782f32
Naveen Kumar Shiraguppi Rudrappa <naveen.shiraguppi@gmail.com>
2024-01-19 18:45:04 +1100

    Create TapeEquilibrium.java

---
+++ TapeEquilibrium.java
@@ -0,0 +1,60 @@
+/*
+
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+
+For example, given:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+the function should return 1, as explained above.
+
+*/
+class Solution {
+    public int solution(int[] A) {
+        // Implement your solution here
+        long totalSum = 0;
+        long sum = 0;
+        long minSum = Long.MAX_VALUE;
+        int minIndex = -1;
+        for(int i = 1; i <= A.length; i++) {
+            totalSum+=A[i-1];
+        }
+        for(int i = 1; i < A.length; i++) {
+            sum += A[i-1];
+            long diff = Math.abs(sum - (totalSum - sum));
+            if(diff < minSum) {
+                minSum = diff;
+                minIndex = i;
+            }
+        }
+        return (int)minSum;
+    }
+}
